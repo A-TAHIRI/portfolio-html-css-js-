0502835849a7c16243671c76a5b04df4f4bbec58
Abdel <113614023+TAHIRI1985@users.noreply.github.com>
2023-10-08 02:02:15 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const toggleMenu = () => {
+    const navigation = document.querySelector('.navigation');
+
+
+    const burgerMenu = document.querySelector(".menu-icon");
+    const src = burgerMenu.getAttribute('src');
+
+    const isBurger = src === './images/carre-1.svg';
+
+    const iconName = isBurger ?
+        './images/close.svg'
+        :
+        './images/carre-1.svg';
+
+
+    burgerMenu.setAttribute(
+        'src',
+        iconName
+    );
+
+    if (!isBurger) {
+        navigation.classList.add("navigation--mobile--fadeout");
+        setTimeout(() => {
+            navigation.classList.toggle(
+                'navigation--mobile'
+            );
+        }, 300)
+    } else {
+        navigation.classList.remove("navigation--mobile--fadeout");
+        navigation.classList.toggle(
+            'navigation--mobile'
+        );
+    }
+};
